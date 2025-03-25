53846cc69cc45ae350c9a1ce3e287610de7dd160
zintiserics <zintis.erics@outlook.com>
2025-03-25 15:26:10 +0200

    Added OSM.

--- darzi-map/src/index.js
+++ darzi-map/src/index.js
@@ -1,12 +1,56 @@
-import { Map } from 'maplibre-gl';
+import { Map, NavigationControl, GeolocateControl } from 'maplibre-gl';
 
 async function main() {
+  // osm map
+  const style = {
+    version: 8,
+    sources: {
+      osm: {
+        type: 'raster',
+        tiles: [
+          'https://a.tile.openstreetmap.org/{z}/{x}/{y}.png',
+          'https://b.tile.openstreetmap.org/{z}/{x}/{y}.png',
+          'https://c.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        ],
+        tileSize: 256,
+        attribution: '&copy; OpenStreetMap Contributors',
+        maxzoom: 19,
+      },
+    },
+    layers: [
+      {
+        id: 'osm',
+        type: 'raster',
+        source: 'osm',
+      },
+    ],
+  };
   const map = new Map({
-    center: [0, 0],
+    center: [25.4281, 57.5417],
     container: 'map',
     hash: true,
-    style: 'https://demotiles.maplibre.org/style.json',
-    zoom: 7,
+    style: style,
+    maxZoom: 19,
+    zoom: 15,
+  });
+  // controls
+  map.addControl(new NavigationControl({
+    visualizePitch: true,
+    visualizeRoll: true,
+    showZoom: true,
+    showCompass: true,
+  }));
+  map.addControl(new GeolocateControl({
+    positionOptions: { enableHighAccuracy: true },
+    trackUserLocation: true,
+  }));
+  // coords
+  const coordsElem = document.getElementById('coords');
+  const center = map.getCenter().wrap();
+  coordsElem.innerHTML = `lon: ${center.lng.toFixed(4)} lat: ${center.lat.toFixed(4)}`;
+  map.on('mousemove', e => {
+    const lngLat = e.lngLat.wrap();
+    coordsElem.innerHTML = `lon: ${lngLat.lng.toFixed(4)} lat: ${lngLat.lat.toFixed(4)}`;
   });
 };
 
