a7a705ebca28d869f8f28aa90ffd9e85d96d3cff
zintiserics <zintis.erics@outlook.com>
2025-03-25 14:19:52 +0200

    Added map.

--- darzi-map/src/index.js
+++ darzi-map/src/index.js
@@ -1,5 +1,13 @@
+import { Map } from 'maplibre-gl';
+
 async function main() {
-  alert("TODO");
+  const map = new Map({
+    center: [0, 0],
+    container: 'map',
+    hash: true,
+    style: 'https://demotiles.maplibre.org/style.json',
+    zoom: 7,
+  });
 };
 
 main();
