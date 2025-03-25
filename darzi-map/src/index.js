cb07ea72d3bbe0d33695b15e929c1ca1477b13d0
zintiserics <zintis.erics@outlook.com>
2025-03-25 18:13:30 +0200

    Updated dependencies.

--- darzi-map/src/index.js
+++ darzi-map/src/index.js
@@ -3,66 +3,61 @@ import { MaplibreTerradrawControl } from '@watergis/maplibre-gl-terradraw';
 
 import { GlassShackGnomeTalk } from './protocol';
 
-function main() {
-  // osm map
-  const style = {
-    version: 8,
-    sources: {
-      osm: {
-        type: 'raster',
-        tiles: [
-          'https://a.tile.openstreetmap.org/{z}/{x}/{y}.png',
-          'https://b.tile.openstreetmap.org/{z}/{x}/{y}.png',
-          'https://c.tile.openstreetmap.org/{z}/{x}/{y}.png',
-        ],
-        tileSize: 256,
-        attribution: '&copy; OpenStreetMap Contributors',
-        maxzoom: 19,
-      },
+// osm map
+const style = {
+  version: 8,
+  sources: {
+    osm: {
+      type: 'raster',
+      tiles: [
+        'https://a.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        'https://b.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        'https://c.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      ],
+      tileSize: 256,
+      attribution: '&copy; OpenStreetMap Contributors',
+      maxzoom: 19,
     },
-    layers: [
-      {
-        id: 'osm',
-        type: 'raster',
-        source: 'osm',
-      },
-    ],
-  };
-  const map = new Map({
-    center: [25.4281, 57.5417],
-    container: 'map',
-    hash: true,
-    style: style,
-    maxZoom: 19,
-    zoom: 15,
-  });
-  // controls
-  map.addControl(new NavigationControl({
-    visualizePitch: true,
-    visualizeRoll: true,
-    showZoom: true,
-    showCompass: true,
-  }));
-  map.addControl(new GeolocateControl({
-    positionOptions: { enableHighAccuracy: true },
-    trackUserLocation: true,
-  }));
-  map.addControl(new MaplibreTerradrawControl({
-    modes: ['rectangle'],
-    open: true,
-  }));
-  // coords
-  const coordsElem = document.getElementById('coords');
-  const center = map.getCenter().wrap();
-  coordsElem.innerHTML = `lon: ${center.lng.toFixed(4)} lat: ${center.lat.toFixed(4)}`;
-  map.on('mousemove', e => {
-    const lngLat = e.lngLat.wrap();
-    coordsElem.innerHTML = `lon: ${lngLat.lng.toFixed(4)} lat: ${lngLat.lat.toFixed(4)}`;
-  });
-  // protocol
-  const gsgt = new GlassShackGnomeTalk();
-  return gsgt;
+  },
+  layers: [
+    {
+      id: 'osm',
+      type: 'raster',
+      source: 'osm',
+    },
+  ],
 };
-
-const gsgt = main();
-gsgt.init();
+const map = new Map({
+  center: [25.4281, 57.5417],
+  container: 'map',
+  hash: true,
+  style: style,
+  maxZoom: 19,
+  zoom: 15,
+});
+// controls
+map.addControl(new NavigationControl({
+  visualizePitch: true,
+  visualizeRoll: true,
+  showZoom: true,
+  showCompass: true,
+}));
+map.addControl(new GeolocateControl({
+  positionOptions: { enableHighAccuracy: true },
+  trackUserLocation: true,
+}));
+map.addControl(new MaplibreTerradrawControl({
+  modes: ['rectangle'],
+  open: true,
+}));
+// coords
+const coordsElem = document.getElementById('coords');
+const center = map.getCenter().wrap();
+coordsElem.innerHTML = `lon: ${center.lng.toFixed(4)} lat: ${center.lat.toFixed(4)}`;
+map.on('mousemove', e => {
+  const lngLat = e.lngLat.wrap();
+  coordsElem.innerHTML = `lon: ${lngLat.lng.toFixed(4)} lat: ${lngLat.lat.toFixed(4)}`;
+});
+// protocol
+window.gsgt = new GlassShackGnomeTalk();
+window.gsgt.init();
