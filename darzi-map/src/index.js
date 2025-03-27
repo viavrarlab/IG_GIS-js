6e85e4a14ec5430377cf7f8c1a06c3c722bc673f
zintiserics <zintis.erics@outlook.com>
2025-03-27 17:06:58 +0200

    Added mapzen.

--- darzi-map/src/index.js
+++ darzi-map/src/index.js
@@ -3,8 +3,10 @@ import { MaplibreTerradrawControl } from '@watergis/maplibre-gl-terradraw';
 
 import { GlassShackGnomeTalk } from './protocol';
 
-// osm map
-const style = {
+
+
+// map
+const styleOSM = {
   version: 8,
   sources: {
     osm: {
@@ -27,14 +29,46 @@ const style = {
     },
   ],
 };
+const styleMapzen = {
+  version: 8,
+  sources: {
+    mapzen: {
+      type: 'raster',
+      tiles: ['https://s3.amazonaws.com/elevation-tiles-prod/terrarium/{z}/{x}/{y}.png'],
+      tileSize: 256,
+      attribution: '&copy; Mapzen',
+      maxzoom: 15,
+    },
+  },
+  layers: [
+    {
+      id: 'mapzen',
+      type: 'raster',
+      source: 'mapzen',
+    },
+  ],
+};
 const map = new Map({
   center: [25.4281, 57.5417],
   container: 'map',
   hash: true,
-  style: style,
+  style: styleOSM,
   maxZoom: 19,
   zoom: 15,
 });
+
+
+
+// layers
+const layers = {
+  osm: styleOSM,
+  mapzen: styleMapzen,
+};
+const layersElem = document.getElementById('layers');
+layersElem.addEventListener('change', e => map.setStyle(layers[e.target.value]));
+
+
+
 // controls
 map.addControl(new NavigationControl({
   visualizePitch: true,
@@ -46,10 +80,14 @@ map.addControl(new GeolocateControl({
   positionOptions: { enableHighAccuracy: true },
   trackUserLocation: true,
 }));
-map.addControl(new MaplibreTerradrawControl({
-  modes: ['rectangle'],
+const terradrawControl = new MaplibreTerradrawControl({
+  modes: ['rectangle', 'select', 'delete-selection', 'delete'],
   open: true,
-}));
+});
+map.addControl(terradrawControl, 'top-left');
+
+
+
 // coords
 const coordsElem = document.getElementById('coords');
 const center = map.getCenter().wrap();
@@ -58,6 +96,15 @@ map.on('mousemove', e => {
   const lngLat = e.lngLat.wrap();
   coordsElem.innerHTML = `lon: ${lngLat.lng.toFixed(4)} lat: ${lngLat.lat.toFixed(4)}`;
 });
+
+
+
 // protocol
 window.gsgt = new GlassShackGnomeTalk();
+const terradraw = terradrawControl.getTerraDrawInstance();
+terradraw.on('finish', id => {
+  const feature = terradraw.getSnapshotFeature(id);
+  console.log(feature);
+  // TODO
+});
 window.gsgt.init();
