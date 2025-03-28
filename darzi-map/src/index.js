46bb56030de19b0e7f7ff4d6477414a197ab218c
zintiserics <zintis.erics@outlook.com>
2025-03-28 12:47:30 +0200

    Added contour.

--- darzi-map/src/index.js
+++ darzi-map/src/index.js
@@ -1,4 +1,6 @@
+import maplibregl from 'maplibre-gl';
 import { Map, NavigationControl, GeolocateControl } from 'maplibre-gl';
+import mlcontour from 'maplibre-contour';
 import { MaplibreTerradrawControl } from '@watergis/maplibre-gl-terradraw';
 
 import { GlassShackGnomeTalk } from './protocol';
@@ -48,6 +50,79 @@ const styleMapzen = {
     },
   ],
 };
+const demMapzen = new mlcontour.DemSource({
+  url: 'https://s3.amazonaws.com/elevation-tiles-prod/terrarium/{z}/{x}/{y}.png',
+  encoding: 'terrarium',
+  maxzoom: 15,
+  worker: true,
+  cacheSize: 1000,
+});
+demMapzen.setupMaplibre(maplibregl);
+const styleContour = {
+  version: 8,
+  glyphs: 'https://demotiles.maplibre.org/font/{fontstack}/{range}.pbf',
+  sources: {
+    hillshade: {
+      type: 'raster-dem',
+      tiles: [demMapzen.sharedDemProtocolUrl],
+      tileSize: 256,
+      maxzoom: 15,
+    },
+    contours: {
+      type: 'vector',
+      tiles: [demMapzen.contourProtocolUrl({
+        overzoom: 1,
+        thresholds: {
+          11: [7, 35],
+          12: [5, 25],
+          13: [3, 15],
+          14: [2, 10],
+          15: [1, 5],
+        },
+        elevationKey: 'elevation',
+        levelKey: 'level',
+        contourLayer: 'contours',
+      })],
+      maxzoom: 15,
+    },
+  },
+  layers: [
+    {
+      id: 'hillshade',
+      type: 'hillshade',
+      source: 'hillshade',
+      layout: { visibility: 'visible' },
+      paint: { 'hillshade-exaggeration': 0.25 },
+    },
+    {
+      id: 'contours',
+      type: 'line',
+      source: 'contours',
+      'source-layer': 'contours',
+      paint: {
+        'line-opacity': 0.5,
+        'line-width': ['match', ['get', 'level'], 1, 1, 0.5],
+      },
+    },
+    {
+      id: 'symbols',
+      type: 'symbol',
+      source: 'contours',
+      'source-layer': 'contours',
+      filter: ['>', ['get', 'level'], 0],
+      paint: {
+        'text-halo-color': 'white',
+        'text-halo-width': 1,
+      },
+      layout: {
+        'symbol-placement': 'line',
+        'text-size': 10,
+        'text-field': ['number-format', ['get', 'elevation'], {}],
+        'text-font': ['Noto Sans Bold'],
+      },
+    },
+  ],
+};
 const map = new Map({
   center: [25.4281, 57.5417],
   container: 'map',
@@ -63,6 +138,7 @@ const map = new Map({
 const layers = {
   osm: styleOSM,
   mapzen: styleMapzen,
+  contour: styleContour,
 };
 const layersElem = document.getElementById('layers');
 layersElem.addEventListener('change', e => map.setStyle(layers[e.target.value]));
