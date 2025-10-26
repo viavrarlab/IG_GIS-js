f7c1a979835118b026759c8d51fe18c669173e5f
zintiserics <zintis.erics@outlook.com>
2025-10-26 14:53:00 +0200

    Updated pages.

--- darzi-map/scripts/pages-del.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -euxo pipefail
-
-rm -rf ../darzi-temp
-rm -rf ../darzi-pages
--- darzi-map/scripts/parcel-build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -euxo pipefail
-
-cross-env NODE_ENV=production parcel build index.html --public-url .
