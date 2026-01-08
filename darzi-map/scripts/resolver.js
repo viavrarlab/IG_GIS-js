b2d8c48780ee1ecf0032831a2903d0b5c2da86a5
zintiserics <zintis.erics@outlook.com>
2026-01-08 18:00:08 +0200

    Added resolver.

---
+++ darzi-map/scripts/resolver.js
@@ -0,0 +1,16 @@
+// const fs = require('fs');
+const path = require('path');
+
+const { Resolver } = require('@parcel/plugin');
+
+module.exports = new Resolver({
+    async resolve({ specifier }) {
+        if (!specifier.startsWith('http') || !specifier.endsWith('.css')) return null;
+        const response = await fetch(specifier);
+        const css = await response.text();
+        const filePath = path.join(...__filename.split(path.sep).slice(0, -2), 'dist', specifier.split('/').at(-1));
+        // fs.mkdirSync(path.dirname(filePath), { recursive: true });
+        // fs.writeFileSync(filePath, css);
+        return { filePath: filePath, css };
+    }
+});
